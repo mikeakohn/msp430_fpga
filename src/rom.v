b6680b8bbb105c5361b9341bce1eb35ac2b512f5
Michael Kohn <mike@mikekohn.net>
2026-06-24 19:19:43 -0500

    ROM doesn't need to be clocked in rom.v.

--- src/rom.v
+++ src/rom.v
@@ -5,14 +5,14 @@
 //   Board: iceFUN iCE40 HX8K
 // License: MIT
 //
-// Copyright 2024 by Michael Kohn
+// Copyright 2024-2026 by Michael Kohn
 
-// This is a hardcoded program that blinks an external LED.
+// This is a hardcoded program.
 
 module rom
 (
   input  [11:0] address,
-  output reg [15:0] data_out,
+  output [15:0] data_out,
   input clk
 );
 
@@ -22,9 +22,13 @@ initial begin
   $readmemh("rom.txt", memory);
 end
 
+/*
 always @(posedge clk) begin
   data_out <= memory[address[11:1]];
 end
+*/
+
+assign data_out = memory[address[11:1]];
 
 endmodule
 
